8e848460ae65cbafcbfd2a9f30e744ad7edb58e6
chamlamh <70345325+chamlamh@users.noreply.github.com>
2021-04-04 23:26:29 -0400

    add Mid

---
+++ ThreeCardsBG/Assets/Scripts/CardModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardModel : MonoBehaviour
+{
+    public Sprite[] faces;
+    public Sprite cardBack;
+
+
+
+}
